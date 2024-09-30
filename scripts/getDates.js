a09b2e9adf79f35beeb5fc30e5d090fe65a2b0f9
issahakuffo <issahakuffo300@gmail.com>
2024-09-30 20:22:29 +0000

    adding css and jb

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -12,3 +12,15 @@ const dateTime = new Date().toLocaleString();;
 last_modified.innerHTML = dateTime;
 
 
+const menu_button = document.querySelector("#Menu_button");
+const mobile_nav = document.querySelector("#Mobile_nav");
+menu_button.addEventListener("click", function () {
+
+    if (mobile_nav.className === '') {
+        mobile_nav.className = 'mobile_menu';
+        menu_button.innerHTML = '<i class="fa fa-times"></i>';
+    } else {
+        mobile_nav.className = '';
+        menu_button.innerHTML = '<i class="fa fa-bars"></i>';
+    }
+})
